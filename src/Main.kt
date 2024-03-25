882f7f877fced8cfa5823cac2c21b6813d44a165
rehema-funani <rehemafunani@gmail.com>
2024-03-25 08:32:02 +0300

    second commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,14 @@
 fun main() {
     mySentence("Barnie bakes brown bagels and buns")
    println(myNumbers(arrayOf(1,2,3,4,5)))
+    myWord("madam")
 
 
 
 }
 fun mySentence(sentence:String){
     println(sentence.replace("b",""))
-    sphere(2)
+    sphere(1)
 
 
 }
@@ -23,7 +24,22 @@ fun myNumbers(numbers:Array<Int>):Int{
 
 
 fun sphere(r:Int){
+    var x = r*3
+    var c = 4/3 *3.14159
+    println(x*c)
 
 
 
+
+}
+fun myWord(word:String) {
+    val palindrome = madam
+
+    if (word == palindrome) {
+        println(True)
+
+    }
+    else {
+        println(False)
+    }
 }
