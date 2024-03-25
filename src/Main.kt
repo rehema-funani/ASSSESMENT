fd73d295b035c66b0d5c5edbfe44bb909d30db1c
rehema-funani <rehemafunani@gmail.com>
2024-03-25 08:13:52 +0300

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,29 @@
+fun main() {
+    mySentence("Barnie bakes brown bagels and buns")
+   println(myNumbers(arrayOf(1,2,3,4,5)))
+
+
+
+}
+fun mySentence(sentence:String){
+    println(sentence.replace("b",""))
+    sphere(2)
+
+
+}
+fun myNumbers(numbers:Array<Int>):Int{
+    return numbers.count()
+
+
+
+}
+
+
+
+
+
+fun sphere(r:Int){
+
+
+
+}
